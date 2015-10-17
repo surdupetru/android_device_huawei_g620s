b7e8748e4d787b0f482cbbb391f5dc9a7144395d
surdupetru <surdu_petru@yahoo.com>
2015-10-17 15:02:08 +0200

    add sensors hal

--- g620s.mk
+++ g620s.mk
@@ -206,6 +206,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8916
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8916 \
+    libcalmodule_akm \
+    calmodule.cfg
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
