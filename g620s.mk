fa5d0e0372e0fc2c9cef9d636c0c753a8b9f6d2d
surdupetru <surdu_petru@yahoo.com>
2015-10-21 18:30:08 +0200

    add antradio permission

--- g620s.mk
+++ g620s.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
 
 # Feature definition files for msm8916
 PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
