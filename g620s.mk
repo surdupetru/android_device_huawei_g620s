e0831f6521147aa693f2d74b5e2a053e2c41c2a5
surdupetru <surdu_petru@yahoo.com>
2015-11-08 00:41:30 +0100

    remove sensors for now

--- g620s.mk
+++ g620s.mk
@@ -180,12 +180,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8916
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.msm8916 \
-    libcalmodule_akm \
-    calmodule.cfg
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
