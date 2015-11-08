b7ebcc66e57d1327a27cf5a36aedd21a2c9defcc
desalesouche <desalesouche@gmail.com>
2015-11-09 00:51:46 +0100

    # Enable dex-preoptimization to speed up first boot sequence

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,3 +184,13 @@ TARGET_USES_WCNSS_CTRL := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+DONT_DEXPREOPT_PREBUILTS := true
