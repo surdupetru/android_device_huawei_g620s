5ae1c77d89fb8e7e4d554f2beb42db0c1fe72656
surdupetru <surdu_petru@yahoo.com>
2015-11-11 20:47:40 +0100

    update partitions on fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,7 @@ TARGET_POWERHAL_VARIANT := qcom
 BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
